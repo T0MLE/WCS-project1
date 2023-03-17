e5e7ba2cd8b4409e9274a5f64393881826e63237
ThomasLonjon <lonjon.thomas@gmail.com>
2023-03-17 11:29:03 +0100

    added flipping cards

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ const fishing = document.querySelector('.imageFishing');
 
 
 football.addEventListener("click", function(){
-    football.classList.toggle("text");
+            football.classList.toggle("text");
+            football.classList.toggle("imageFootball");
+    
 })
 
 videoGame.addEventListener("click", function(){
     videoGame.classList.toggle("text");
+    videoGame.classList.toggle("imageVideoGame");
+
 })
 
 fishing.addEventListener("click", function(){
     fishing.classList.toggle("text");
+    fishing.classList.toggle("imageFishing");
 })
+
+
