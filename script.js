f67ce2cc818a6a6eb40adfb55f9370cbf0eb36c9
Omci <melloulchi.omar@gmail.com>
2023-03-17 11:29:11 +0100

    Homepage creation

---
+++ script.js
@@ -0,0 +1,71 @@
+
+const menu = document.querySelector('.menu');
+const contact = document.querySelector('.contact-me')
+
+
+// CHECKBOX
+  const toggleTheme = document.querySelector('#toggle-checkbox');
+  const button = document.querySelector('.toggle-theme span');
+  
+  
+  toggleTheme.addEventListener('change', function() {
+      button.classList.toggle('span-move');
+      document.body.classList.toggle('blue-theme');
+      menu.classList.toggle('blue-theme-menu');
+      if (document.body.classList.contains('blue-theme')) {
+        contact.classList.add('blue-theme-font');
+        contact.classList.remove('red-theme-font')
+        button.classList.add('button-color-blue');
+        button.classList.remove('button-color-red');
+      } else {
+        contact.classList.remove('blue-theme-font');
+        contact.classList.add('red-theme-font')
+        button.classList.add('button-color-red');
+        button.classList.remove('button-color-blue');
+      }
+  });
+
+
+
+//   BURGER MENU
+// const menuBtn = document.querySelector('.menu-btn');
+
+// let isOpen = false;
+
+// menuBtn.addEventListener('click', () => {
+//   isOpen = !isOpen;
+
+//   if (isOpen) {
+//     menuBtn.classList.add('open');
+//     menu.style.height = '150px';
+//   } else {
+//     menuBtn.classList.remove('open');
+//     menu.style.height = '0';
+//   }
+// });
+
+
+const menuBtn = document.querySelector('.menu-btn');
+let isOpen = false;
+menuBtn.addEventListener('click', function() {
+  isOpen = !isOpen;
+  if (isOpen) {
+    menuBtn.classList.add('open');
+    menu.style.height = '150px';
+  } else {
+    menuBtn.classList.remove('open');
+    menu.style.height = '0';
+  }
+});
+
+
+
+function playAudio() {
+   var audio = document.getElementById("myAudio"); 
+   audio.play();
+   }
+
+   function playAudio2() {
+    var audio1 = document.getElementById("myAudio2");
+    audio1.play();
+    }
